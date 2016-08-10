b1793e65953a68d597a6b93a413c17758bede35a
Alexis Lopez Zubieta <azubieta@uci.cu>
2016-08-09 20:40:10 -0400

    Creating project structure.

---
+++ Definitions.cmake
@@ -0,0 +1,15 @@
+
+# Define project your name
+SET(Project "sddm-core-theme")
+
+# Project description
+SET(Project_desc "Sddm theme for the Plasma Light project.")
+
+# Set copyright info
+SET(Copyright "PlasmaLight")
+
+# Set Qt minimal version
+SET(Qt_min_REQUIRED 5.2.0)
+
+# Define project version x.y.z
+SET(ver 0.0.1)
